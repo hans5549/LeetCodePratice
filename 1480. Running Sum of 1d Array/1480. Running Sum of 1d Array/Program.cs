bbc5da77e54a7cd005cb21f2204c5f7baead7cc1
hans <s10323225@gmail.com>
2022-07-20 11:59:38 +0800

    Problem 1480

---
+++ 1480. Running Sum of 1d Array/1480. Running Sum of 1d Array/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _1480._Running_Sum_of_1d_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
