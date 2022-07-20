6f7455d13e3037ae9393e36fa0f90a86b03e1fb7
hans <s10323225@gmail.com>
2022-07-20 13:41:06 +0800

    Create 724. Find Pivot Index

---
+++ 724. Find Pivot Index/724. Find Pivot Index/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _724._Find_Pivot_Index
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
