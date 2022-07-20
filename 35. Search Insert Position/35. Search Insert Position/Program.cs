3e7340558d57c3d1bccf5dcbcab5033fe0e9ad8b
hans <s10323225@gmail.com>
2022-07-20 16:09:16 +0800

    Success

---
+++ 35. Search Insert Position/35. Search Insert Position/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _35._Search_Insert_Position
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 3, 5, 6 };
+            int index = SearchInsert(nums, 5);
+        }
+
+        public static int SearchInsert(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == target || nums[i] > target)
+                {
+                    return i;
+                }
+            }
+
+            return nums.Length;
+        }
+    }
+}
