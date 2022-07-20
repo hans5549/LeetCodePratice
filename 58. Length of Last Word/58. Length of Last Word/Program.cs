04a7cd83801b34726366dd0c4907723042ef9f9a
hans <s10323225@gmail.com>
2022-07-20 16:34:54 +0800

    Problem 58 success

---
+++ 58. Length of Last Word/58. Length of Last Word/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+
+namespace _58._Length_of_Last_Word
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = LengthOfLastWord("   fly me   to   the moon  ");
+        }
+
+        public static int LengthOfLastWord(string s)
+        {
+            string[] stringArray = s.Replace(@"\s+", " ").Split(" ").Where(x => x.Length > 0).ToArray();
+
+            return stringArray[stringArray.Length - 1].Length;
+        }
+    }
+}
