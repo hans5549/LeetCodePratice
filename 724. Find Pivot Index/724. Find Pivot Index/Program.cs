29ea416cf749dabd2bb47968494b4e63b1f1d34a
hans <s10323225@gmail.com>
2022-07-20 14:03:13 +0800

    Success

--- 724. Find Pivot Index/724. Find Pivot Index/Program.cs
+++ 724. Find Pivot Index/724. Find Pivot Index/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _724._Find_Pivot_Index
 {
@@ -6,7 +7,23 @@ namespace _724._Find_Pivot_Index
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] nums = { 1, 7, 3, 6, 5, 6 };
+            int index = PivotIndex(nums);
+    }
+
+        public static int PivotIndex(int[] nums)
+        {
+            int sumA = 0;
+            int sumB = nums.Sum();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                sumB -= nums[i];
+                if (sumA == sumB) return i;
+                else sumA += nums[i];
+            }
+
+            return -1;
         }
     }
 }
