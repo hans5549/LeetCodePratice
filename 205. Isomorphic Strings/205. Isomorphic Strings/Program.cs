ed21e55a7b5bf9d585175ba42eb26a48dc628ab8
hans <s10323225@gmail.com>
2022-07-21 16:22:09 +0800

    problem 205 success

---
+++ 205. Isomorphic Strings/205. Isomorphic Strings/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _205._Isomorphic_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "egg";
+            string t = "add";
+            bool o = IsIsomorphic(s, t);
+        }
+
+        public static bool IsIsomorphic(string s, string t)
+        {
+            List<int> a = GetCharIndexList(s);
+            List<int> b = GetCharIndexList(t);
+
+            if (Enumerable.SequenceEqual(a, b))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private static List<int> GetCharIndexList(string c)
+        {
+            List<int> indexList = new List<int>();
+            List<char> chartemp = new List<char>();
+            foreach (char t in c.ToCharArray())
+            {
+                int index = chartemp.IndexOf(t);
+                if (index != -1)
+                {
+                    indexList.Add(index);
+                }
+                else
+                {
+                    chartemp.Add(t);
+                    index = chartemp.IndexOf(t);
+                    indexList.Add(index);
+                }
+            }
+
+            return indexList;
+        }
+    }
+}
