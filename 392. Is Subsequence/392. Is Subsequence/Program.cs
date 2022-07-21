393d91358d6fb823a73f07c8455f056daf5b2fa1
hans <s10323225@gmail.com>
2022-07-21 17:21:43 +0800

    problem 392 success

---
+++ 392. Is Subsequence/392. Is Subsequence/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _392._Is_Subsequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "cc";
+            string t = "bc";
+            bool o = IsSubsequence(s, t);
+        }
+
+        public static bool IsSubsequence(string s, string t)
+        {
+            if (s.Length == 0)
+            {
+                return true;
+            }
+
+            char[] sArray = s.ToCharArray();
+            char[] tArray = t.ToCharArray();
+            int sIndex = 0;
+            for (int tIndex = 0; tIndex < t.Length; ++tIndex)
+            {
+                if (sArray[sIndex] == tArray[tIndex])
+                {
+                    ++sIndex;
+                }
+
+                if (sIndex == s.Length)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
