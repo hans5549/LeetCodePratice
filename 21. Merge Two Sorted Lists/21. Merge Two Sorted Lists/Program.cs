67249f777328e8c39398a42d9d5ab5c00a6926a4
hans <s10323225@gmail.com>
2022-07-22 09:15:27 +0800

    problem 21

---
+++ 21. Merge Two Sorted Lists/21. Merge Two Sorted Lists/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _21._Merge_Two_Sorted_Lists
+{
+    class Program
+    {
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int val = 0, ListNode next = null)
+            {
+                this.val = val;
+                this.next = next;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static ListNode MergeTwoLists(ListNode list1, ListNode list2)
+        {
+            if (list1 == null)
+            {
+                return list2;
+            }
+            else if (list2 == null)
+            {
+                return list1;
+            }    
+            else
+            {
+                if (list1.val < list2.val)
+                {
+                    list1.next = MergeTwoLists(list1.next, list2);
+                    return list1;
+                }
+                else
+                {
+                    list2.next = MergeTwoLists(list1, list2.next);
+                    return list2;
+                }
+            }
+        }
+    }
+}
