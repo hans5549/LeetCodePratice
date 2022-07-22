4f0df838566d0a4eafab2f2e58ba23e7c7a3937b
hans <s10323225@gmail.com>
2022-07-22 09:25:21 +0800

    problem 206

---
+++ 206. Reverse Linked List/206. Reverse Linked List/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _206._Reverse_Linked_List
+{
+    class Program
+    {
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+
+            public ListNode(int val = 0, ListNode next = null)
+            {
+                this.val = val;
+                this.next = next;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static ListNode ReverseList(ListNode head)
+        {
+            if (head == null || head.next == null)
+            {
+                return head;
+            }
+
+            ListNode res = ReverseList(head.next);
+            head.next.next = head;
+            head.next = null;
+
+            return res;
+        }
+    }
+}
