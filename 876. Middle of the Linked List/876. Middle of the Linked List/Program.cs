9d8b37645a1bf4e054966b7f9d93d2fd739f085e
hans <s10323225@gmail.com>
2022-07-28 09:47:58 +0800

    problem 876 success

---
+++ 876. Middle of the Linked List/876. Middle of the Linked List/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _876._Middle_of_the_Linked_List
+{
+    class Program
+    {
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int val = 0, ListNode next = null)
+            {
+                this.val = val;
+                this.next = next;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+        }
+
+        public ListNode MiddleNode(ListNode head)
+        {
+            ListNode pSlow = head, pFast = head;
+            while (pFast != null && pFast.next != null)
+            {
+                pFast = pFast.next.next;
+                pSlow = pSlow.next;
+            }
+            return pSlow;
+        }
+    }
+}
